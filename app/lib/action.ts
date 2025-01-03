4ca48267d3afc21b9b7d87e5090a7af7775de5dc
haht93 <haht.fe@gmail.com>
2025-01-03 11:13:12 +0700

    Create invoice

---
+++ app/lib/action.ts
@@ -0,0 +1,28 @@
+'use server';
+
+// import { Invoice } from '@/app/types'; // Adjust the import path
+import { sql } from '@vercel/postgres'; // Adjust based on your database library
+import { revalidatePath } from 'next/cache'
+import { redirect } from 'next/navigation';
+
+export async function createInvoice(formData: FormData) {
+  const customer_id = formData.get('customerId') as string;
+  const amount = parseFloat(formData.get('amount') as string);
+  const status = formData.get('status') as 'pending' | 'paid';
+
+  // Validate the data
+  if (!customer_id || !amount || isNaN(amount) || amount <= 0 || !status) {
+    throw new Error('Invalid form data');
+  }
+
+  const amountInCents = Math.round(amount * 100);
+  const date = new Date().toISOString().split('T')[0];
+
+  // Insert into the database
+  await sql`
+    INSERT INTO invoices (customer_id, amount, status, date)
+    VALUES (${customer_id}, ${amountInCents}, ${status}, ${date})
+  `;
+  revalidatePath('/dashboard/invoices');
+  redirect('/dashboard/invoices');
+}
--- app/ui/invoices/create-form.tsx
+++ app/ui/invoices/create-form.tsx
@@ -1,4 +1,5 @@
 import { CustomerField } from '@/app/lib/definitions';
+import { createInvoice } from '@/app/lib/action';
 import Link from 'next/link';
 import {
   CheckIcon,
@@ -10,7 +11,7 @@ import { Button } from '@/app/ui/button';
 
 export default function Form({ customers }: { customers: CustomerField[] }) {
   return (
-    <form>
+    <form action={createInvoice}>
       <div className="rounded-md bg-gray-50 p-4 md:p-6">
         {/* Customer Name */}
         <div className="mb-4">
@@ -21,8 +22,8 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
             <select
               id="customer"
               name="customerId"
+              required
               className="peer block w-full cursor-pointer rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
-              defaultValue=""
             >
               <option value="" disabled>
                 Select a customer
@@ -50,6 +51,7 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
                 type="number"
                 step="0.01"
                 placeholder="Enter USD amount"
+                required
                 className="peer block w-full rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
               />
               <CurrencyDollarIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
@@ -70,6 +72,7 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
                   name="status"
                   type="radio"
                   value="pending"
+                  required
                   className="h-4 w-4 cursor-pointer border-gray-300 bg-gray-100 text-gray-600 focus:ring-2"
                 />
                 <label
@@ -85,6 +88,7 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
                   name="status"
                   type="radio"
                   value="paid"
+                  required
                   className="h-4 w-4 cursor-pointer border-gray-300 bg-gray-100 text-gray-600 focus:ring-2"
                 />
                 <label
