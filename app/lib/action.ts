5a25d47230d8ffb0b343052fd696ad81526e5e00
haht93 <haht.fe@gmail.com>
2025-01-03 16:04:43 +0700

    Edit invoice

--- app/lib/action.ts
+++ app/lib/action.ts
@@ -26,3 +26,25 @@ export async function createInvoice(formData: FormData) {
   revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices');
 }
+
+export async function updateInvoice(id: string, formData: FormData) {
+  const customer_id = formData.get('customerId') as string;
+  const amount = parseFloat(formData.get('amount') as string);
+  const status = formData.get('status') as 'pending' | 'paid';
+
+  // Validate the data
+  if (!customer_id || !amount || isNaN(amount) || amount <= 0 || !status) {
+    throw new Error('Invalid form data');
+  }
+
+  const amountInCents = Math.round(amount * 100);
+
+  // Insert into the database
+  await sql`
+    UPDATE invoices
+    SET customer_id = ${customer_id}, amount = ${amountInCents}, status = ${status}
+    WHERE id = ${id}
+  `;
+  revalidatePath('/dashboard/invoices');
+  redirect('/dashboard/invoices');
+}
